e1d9ae3a75d72bc4e9452baed0874d907dae7a9c
iradu <ionut.radu@tremend.com>
2019-09-02 10:52:08 +0300

    environ

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
+WORKDRI="/work/webpage/webpage/"
 if [[ -n $DEBUG ]]; then
-   echo "DEBUG=$DEBUG" > .env
+   echo "DEBUG=$DEBUG" > $WORKDIR.env
 fi
 
 if [[ -n $ALLOWED_HOSTS ]]; then
-   echo "ALLOWED_HOSTS=$ALLOWED_HOSTS" >> .env
+   echo "ALLOWED_HOSTS=$ALLOWED_HOSTS" >> $WORKDIR.env
 fi
 
 
 django-admin startproject webpage
-cp /work/settings.py /work/webpage/webpage/settings.py
+cp /work/settings.py $WORKDIRsettings.py
 cd webpage
 pythons manage.py startapp mng
 python manage.py runserver 0.0.0.0:8000
