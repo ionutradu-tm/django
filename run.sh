45a51f8e6bf21d53e1f0a3a8ac2ba8c4ec4d12cb
iradu <ionut.radu@tremend.com>
2019-09-02 10:32:24 +0300

    environ

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 
+if [[ -n $DEBUG ]]; then
+   echo "DEBUG=$DEBUG" > .env
+fi
+
+if [[ -n $ALLOWED_HOSTS ]]; then
+   echo "ALLOWED_HOSTS=$ALLOWED_HOSTS" >> .env
+fi
+
+
 django-admin startproject webpage
+cp /work/settings.py /work/webpage/webpage/settings.py
 cd webpage
 pythons manage.py startapp mng
 python manage.py runserver 0.0.0.0:8000
