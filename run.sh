268e6e0c9b2eb48bd35de4b1200c3412ef827faa
iradu <ionut.radu@tremend.com>
2019-09-03 13:21:16 +0300

    add urls.py

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ if [[ -n $WERCKER_TOKEN ]]; then
 fi
 
 cp /work/settings.py $WORKDIR"settings.py"
-cp /work/urls.py  $WORKDIR"urls.py:
+cp /work/urls.py  $WORKDIR"urls.py"
 cd webpage
 cp -r /work/mng /work/webpage/
 python manage.py runserver 0.0.0.0:8000
