4e742344e4f0782d5a1ceebaafa4f7a6d3fbb152
Ionut Radu <ionut.radu@tremend.com>
2020-10-14 21:55:25 +0300

    add train # size=100

--- mng/urls.py
+++ mng/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('deploy', views.deploy, name='deploy'),
-    path('create_branch', views.create_branch, name='create_branch'),
+    path('deploy/', views.deploy, name='deploy'),
+    path('create_branch/', views.create_branch, name='create_branch'),
     path('functional_tests', views.functional_tests, name='functional_tests'),
     path('train', views.train, name='train'),
 ]
