fdada0429852c78cb21d34b4c1e22bfce8292f44
alex-simi <alexandru.simionescu@tremend.com>
2022-08-04 19:55:41 +0300

    testing new view

--- mng/urls.py
+++ mng/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('deploy/', views.deploy, name='deploy'),
-    path('create_branch/', views.create_branch, name='create_branch'),
     path('functional_tests', views.functional_tests, name='functional_tests'),
+    path('test_stress', views.test_stress, name='test_stress'),
     path('train', views.train, name='train'),
-    path('vm_power', views.vm_power, name='vm_power'),
     path('replica', views.replica, name='replica'),
     path('debug', views.debug, name='debug'),
+    path('create_branch/', views.create_branch, name='create_branch'),
+    path('vm_power', views.vm_power, name='vm_power'),
 ]
